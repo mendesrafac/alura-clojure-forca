0afe04475f433803e180a2b8b9ab8c4633745f56
Rafael Mendes <mendesrafa.c@gmail.com>
2016-12-01 20:07:11 -0200

    projeto base
    usando lein

---
+++ test/alura_clojure_forca/core_test.clj
@@ -0,0 +1,7 @@
+(ns alura-clojure-forca.core-test
+  (:require [clojure.test :refer :all]
+            [alura-clojure-forca.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
